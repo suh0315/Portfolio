d1f8f4f98d4d62330f58d563045fb7400e74d445
suh0315 <suhyung0315@gmail.com>
2026-02-13 16:49:03 +0900

    11111111

--- js/main.js
+++ js/main.js
@@ -1,152 +1,85 @@
 $(document).ready(function(){
-	window.onload = function(){
-     const elm = document.querySelectorAll('.section');
-     const elmCount = elm.length;
-     elm.forEach(function(item, index){
-       item.addEventListener('mousewheel', function(event){
-         event.preventDefault();
-         let delta = 0;
+	// 시작 :: visual swiper
+    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
-         if (!event) event = window.event;
-         if (event.wheelDelta) {
-             delta = event.wheelDelta / 120;
-             if (window.opera) delta = -delta;
-         } 
-         else if (event.detail)
-             delta = -event.detail / 3;
+		autoplay: {  /* 팝업 자동 실행 */
+			delay: 5000,
+			disableOnInteraction: true,
+		},
 
-         let moveTop = window.scrollY;
-         let elmSelector = elm[index];
+		effect: "fade", /* fade 효과 */
 
-         // wheel down : move to next section
-         if (delta < 0){
-           if (elmSelector !== elmCount-1){
-             try{
-               moveTop = window.pageYOffset + elmSelector.nextElementSibling.getBoundingClientRect().top;
-             }catch(e){}
-           }
-         }
-         
-         // wheel up : move to previous section
-         else{
-           if (elmSelector !== 0){
-             try{
-               moveTop = window.pageYOffset + elmSelector.previousElementSibling.getBoundingClientRect().top;
-             }catch(e){}
-           }
-         }
+		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
-         const body = document.querySelector('html');
-         window.scrollTo({top:moveTop, left:0, behavior:'smooth'});
-       });
-     });
-   }
-   var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 1,  //초기값 설정 모바일값이 먼저!!
-        spaceBetween: 10,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        breakpoints: {
-        
-          768: {
-            slidesPerView: 2,  //브라우저가 768보다 클 때
-            spaceBetween: 40,
-          },
-          1024: {
-            slidesPerView: 5,  //브라우저가 1024보다 클 때
-            spaceBetween: 50,
-          },
-        },
-     centeredSlides: true,
-     watchOverflow : true,
-     allowTouchMove : true,
-     autoplay: {
-       delay: 2500,
-       speed: 600,
-       disableOnInteraction: false,
-     },
-     pagination: {
-       el: ".swiper-pagination",
-       clickable: true,
-     },
-     navigation: {
-       nextEl: ".swiper-button-next",
-       prevEl: ".swiper-button-prev",
-     },
-   });
+		pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+			el: '.visual .paging ul', /* 해당 요소의 class명 */
+			clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+		},
 
-   const targets = document.querySelectorAll(".fade-class");
-   const options = { root: null, threshold: 0.1, rootMargin: "-0px" };
-   const observer = new IntersectionObserver(function (entries, observer) {
-   entries.forEach((entry) => {
-     const container = entry.target;
-     if (entry.isIntersecting) {
-       container.classList.add("fade-in");
-     } else {
-       container.classList.remove("fade-in");
-     }
-   });
-   }, options);
+	});
+	// 끝 :: visual swiper
 
-   targets.forEach((target) => {
-   observer.observe(target);
-   });
+	// 시작 :: showing swiper
+	const showing_swiper = new Swiper('.showing .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+			1025: {    /* 1025px 이상일때 적용 */
+				slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
+			},
+			768: {    /* 640px 이상일때 적용 */
+				slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
+			},
+			640: {    /* 640px 이상일때 적용 */
+				slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
+			},
+		},
+		//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+	});
 
-   $('.tab_head button').on('click', function() {
-      let tab = $(this).data('tab');
+	// 끝 :: showing swiper
 
-      $('.tab_head button').removeClass('on');
-      $(this).addClass('on');
 
-      $('.tab_item').removeClass('on');
-      $(`.tab_item[data-tab="${tab}"`).addClass('on');
-   });
+	// 시작 :: event swiper
+	const event_swiper = new Swiper('.event .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
-   import { useRef, useCallback, useEffect } from 'react'
+		autoplay: {  /* 팝업 자동 실행 */
+			delay: 2500,
+			disableOnInteraction: true,
+		},
+	});
+	// 끝 :: event swiper
 
-   // 스크롤하면 페이드인 하는 애니메이션
-   export const useScrollFadeIn = (direction, duration, delay) => {
-   // 해당 컴포넌트 가져오기
-   const element = useRef()
-   // direction 선택
-   const hanldeDirection = (name) => {
-      switch (name) {
-         case "up":
-            return "translate3d(0, 50%, 0)"
-         case "down": return "tranlate3d(0, -50%, 0)"
-         case "left": return "translate3d(50%, 0, 0)"
-         case "right": return "translate3d(-50%, 0, 0)"
-         derault:
-         return;
-      }
-   }
+	// 시작 :: collection swiper
+	const collection_swiper = new Swiper('.collection .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+			769: {    /* 640px 이상일때 적용 */
+				slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
+			},
+		},
+		//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		// loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+		// autoplay: {  /* 팝업 자동 실행 */
+		// 	delay: 2500,
+		// 	disableOnInteraction: true,
+		// },
+		navigation: {
+			nextEl: '.collection .ctrl_btn .next',
+			prevEl: '.collection .ctrl_btn .prev',
+		},
+		pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+			el: '.collection .paging', /* 해당 요소의 class명 */
+			clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+			type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+		},
+	});
 
-   // 설정해둔 컴포넌트를 만날때마다 함수가 재실행되도록 callback하기
-   const onScroll = useCallback(([entry])=>{
-      const { current } = element
-      if(entry.isIntersecting) {
-         current.style.tansitionProperty = 'all'
-         current.style.transitionDuration = `${duration}s`
-         current.style.transitionTimingFunction = "cubic-bezier(0, 0, 0.2, 1)"
-         current.style.opacity = 1;
-         current.style.tranform = "translate3d(0, 0, 0)"
-      }
-   }, [delay, duration])
-
-   // intersection-observer로 컴포넌트 위치 observe하기
-   useEffect(()=>{
-      let observer
-      
-      if(element.current) {
-         observer = new IntersectionObserver(onScroll, {threshold: 0.7})
-         observer.observe(element.current)
-         }
-         
-         return() => observer && observer.disconnect()
-   }, [onScroll])
-
-   return{ref: element, style: { opacity: 0, transform: handleDirection(direction) },
+	// 끝 :: collection swiper
 
 })//맨끝
